7ec3c1473a2f311a128000bb78c887609c8bd6ff
dts2008 <d.tsibulya@platipusgaming.com>
2022-06-29 00:34:56 +0300

    Add springboot

---
+++ src/main/java/cource/task04/wheeloffortune/WheeloffortuneApplication.java
@@ -0,0 +1,14 @@
+package cource.task04.wheeloffortune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheeloffortuneApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(WheeloffortuneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cource/task04/wheeloffortune/WheeloffortuneApplicationTests.java
@@ -0,0 +1,13 @@
+package cource.task04.wheeloffortune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheeloffortuneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
